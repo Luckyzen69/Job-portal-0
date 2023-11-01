50ede719aed1fe1a98062429e54a66564ef3c9f4
Lagzen Thakuri <thakurizen2@gmail.com>
2023-11-01 21:49:27 +0545

    responsive errors

--- src/components/SearchResultlist.jsx
+++ src/components/SearchResultlist.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import SearchResult from "./searchresult"
 export const SearchResultList= ({results})=>{
     return <>
-    <div className="flex flex-col bg-white">
+    <div className="flex flex-col bg-white text-blue-500 ">
     {results.map((result,id)=>{
-            return <SearchResult result={result} key={id}/>
+            return <SearchResult result={result} key={id} />
         })}
     </div>
     </>
--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -10,7 +10,7 @@ export default function Banner() {
          <div className=" lg:grid-cols-1 flex justify-between relative lg:overflow-hidden  bg-light  bg-luffy  h-screen w-auto bg-no-repeat bg-fixed bg-center sm:bg-right lg:bg-right">
       <div className="m-11 p-10 ">
         {/* Adding text to Banner  */}
-        <h2 className=" text-7xl  tracking-wide font-bold sm:text-lightpurple text-lighpurple md:text-black lg:text-black" >
+        <h2 className=" text-7xl  tracking-wide font-bold sm:text-lightpurple text-green md:text-black lg:text-black" >
         Find<span className="text-primary tracking-wide	"> Job</span> That <br />  <span className="text-primary tracking-wide	">Matches </span>  Your <br /> Passion
         </h2>
         <p className="text-third">Hand-picked opportunities to work from home, remotely, freelance,<br /> full-time, part-time, contract and internships.</p>
