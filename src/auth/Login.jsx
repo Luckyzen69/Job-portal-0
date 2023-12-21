a146e8bd474638a9679aa65744a8b9702273b255
Lagzen Thakuri <thakurizen2@gmail.com>
2023-12-21 21:50:00 +0545

    adding role in login

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -13,16 +13,21 @@ export default function Login({setProgress}){
    
     <div className="">
         <h2 className="text-2xl m-2 p-4 text-center bg-light " >Login</h2>
-        <div className=" justify-center items-center flex m-4 p-4">
-        <form  className="grid m-4 justify-center border p-10 font-sans    ">
-            <label htmlFor="login" className="font-bold flex">Username</label>
-            <input type="Username" placeholder="Username*" className="border flex m-2 p-2 lg:w-96 " />
+        <div className=" justify-center items-center flex m-2 p-4">
+        <form  className="grid m-2 justify-center border rounded p-6 font-sans">
+            <label htmlFor="login" className="font-bold flex">Email</label>
+            <input type="email" placeholder="email*" className="border flex m-2 p-2 lg:w-96 " />
             <label htmlFor="password" className="font-bold flex" >Password</label>
             <input type="password" name="password" id="password" placeholder="Password*" className="border flex m-2 mb-4 p-2 " />
+            <label className="font-bold">Role</label>
+            <select name="role" id="" className="m-2 p-2 border ">
+                <option value="">Applicant</option>
+                <option value="">Recuiter</option>
+            </select>
             <Link to="/error" className="border flex hover:ring-2 bg-primary text-white text-center p-2 text-xl">Login</Link>
             <Link to="/error" className="font-sans p-2 m-2">Not a member? <Link to="/register" className="font-bold"> register</Link></Link>
         </form>
         </div>
     </div>
     </>
-}
+}   
