6c0f79db13cabd49173dd87ee6740d5b67af457a
Lagzen Thakuri <thakurizen2@gmail.com>
2023-12-23 00:26:29 +0545

    Update FAQ section with role-wise questions and a main questions section. (generated by blackbox.ai )

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -5,21 +5,27 @@ import { HiBuildingOffice2 } from "react-icons/hi2";
 export default function FAQ(){
     return<>
     <p className="text-xl text-gray-600 text-center">Please share your identity for assistance</p>
-    <div className="flex justify-center m-4">
-        <div className="border m-4 p-4 flex-row items-center list-outside ">
+        {/*  Role wise questions */}
+    <div className="flex justify-center m-4 ">
+        <div className="border m-4 p-4 flex-row items-center list-outside hover:shadow-xl border-sky-600">
             <RiUserSearchLine size={34} color="#5B7FFF" className=" m-4 float-left" />
             <h2 className="font-bold text-sky-600 text-xl =">JobSeeker</h2>
-            <p className="text-sky-400">Looking for job, find answer  to your quries</p>
+            <p className="text-sky-400">Looking for job, find answer  to your queries</p>
         </div>
-        <div className="border m-4 p-4 flex-row items-center list-outside ">
+        <div className="border m-4 p-4 flex-row items-center list-outside  hover:shadow-xl border-sky-900">
             <HiBuildingOffice2 size={34} color="#002a5b" className="m-4 float-left"/>
             <h2 className="font-bold text-sky-900 text-xl"> Employers</h2>
-            <p className="text-sky-900">All about employers account & merojob employer services</p>
+            <p className="text-sky-900">All about employers account & employer services</p>
         </div>
     </div>
+                          {/*  Main Questions */}
+    <h2 className="text-gray-400 text-center font-bold text-2xl underline underline-offset-8 hover:text-gray-800"> Most Popular FAQs</h2>
+    <div className="flex">
 
-    <h2 className="text-gray-400 text-center font-bold text-2xl underline underline-offset-4"> Most Popular FAQs</h2>
+    <div className="border m-2 p-2">
+       <p>How much do you charge for placement?</p>
+    </div>
     
-    
+    </div>
     </>
 }
