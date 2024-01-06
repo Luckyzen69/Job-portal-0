4858e9a6af582a2f93fc24a2801dae7b191dbc44
Lagzen Thakuri <thakurizen2@gmail.com>
2024-01-06 22:02:58 +0545

    Update links and remove unnecessary code in the header component.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -18,20 +18,17 @@ export default function Header() {
         <div>
         <Link to="/home" className=" font-mono font-bold text-3xl has-tooltip hover:text-pink  ">JobsPortal <span className="tooltip rounded shadow-lg p-1 font-normal mt-8 text-sm text-yellow">go to home</span></Link>
         <Link to="/FAQ" className=" hover:text-pink" >FAQ</Link> 
-        <p>{location.status?.id}</p>
+  
         <p>{user && user.name }</p>
         <p>{user && user.email }</p>
         </div>
         <div className=" flex flex-wrap gap-px text-xl">
     
-         
           <>
-<Link to="/login"  className="mt-3 sm:mr-4 ml-3 hover:text-pink"> Login</Link>
-<Link to="/register" className="border border-1 rounded-md p-2 ml-1 bg-primary text-white hover:bg-pink  ">Register</Link>
-          </>
-
-         
           <Link to="/charts" className="mt-3 ml-10 sm:mr-2 hover:text-pink ">Charts</Link>
+          <Link to="/login"  className="mt-3 sm:mr-4 ml-3 hover:text-pink"> Login</Link>
+          <Link to="/register" className="border border-1 rounded-md p-2 ml-1 bg-primary text-white hover:bg-pink  ">Register</Link>
+          </>
           
         </div>
       </div>
