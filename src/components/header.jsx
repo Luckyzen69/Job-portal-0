a87c3d3d253e84f35bce527c335d20c11f2a633d
Lagzen Thakuri <thakurizen2@gmail.com>
2024-01-21 22:41:02 +0545

    Update to get user info

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-  
+      //getting user inf
 function getUser(){
   let user =  localStorage.getItem('user');
   if(user){
